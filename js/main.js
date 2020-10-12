ba21582683556c2a63e91eedd7733b9533ca250f
Miquéias Feitosa silva <72470883+Mikastnn@users.noreply.github.com>
2020-10-12 16:06:18 -0300

    primeiros passos javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Author:Miquêias Feitosa Silva
+
+var nome = "Miquêias Feitosa Silva";
+var idade = 25;
+var idade2 = 5;
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem Vindo "+nome+" sua idade é: "+idade);
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade+idade2);
+alert(n1*n2);
+console.log(n1*n2);
+//console.log(frase);
+console.log(frase.toUpperCase());/*deixando maiscula tudo com o toUpperCase(), tolowecause deixa em caixa baixa*/
+//console.log(frase.replace("Japão", "Brasil"));/*utilizando o replace para substituir palavras*/
+alert(frase.replace("Japão","Brasil"));
